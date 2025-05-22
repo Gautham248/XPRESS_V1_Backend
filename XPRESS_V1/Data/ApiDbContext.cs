e0ceec4cb88fa2d6c2308138deae0dd61cb02b6e
advait2102 <advaitanilkumar7@gmail.com>
2025-05-22 12:31:11 +0530

    table advait

--- XPRESS_V1/Data/ApiDbContext.cs
+++ XPRESS_V1/Data/ApiDbContext.cs
@@ -9,5 +9,6 @@ namespace XPRESS_V1_Backend.Data
         {
         }
         public DbSet<TestTable> TestTables { get; set; } 
+        public DbSet<Advait> Advait { get; set; }
     }
 }
---
+++ XPRESS_V1/Models/Advait.cs
@@ -0,0 +1,8 @@
+﻿namespace XPRESS_V1_Backend.Models
+{
+    public class Advait
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
